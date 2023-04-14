a9e51cbce6208f8069386eccddffc2bd8bbbf829
Lycoris Radiata <lycoris.radiata.mofumofu@gmail.com>
2023-04-14 18:07:41 +0200

    Create a simple window

--- gui/main.c
+++ gui/main.c
@@ -6,11 +6,31 @@
 
 static void app_activate(GApplication* app, gpointer user_data)
 {
-    g_print("The \"%s\" GtkApplication is activated.\n", (char*)user_data);
+    char* app_name = (char*)user_data;
+    GtkWidget* window;
+
+    /*
+        Create a window and connect it to the app:
+        When the window is closed, the app terminates too.
+        If the app is not linked to a window, it has nothing to wait for and terminates immediately.
+    */
+    window = gtk_application_window_new(GTK_APPLICATION(app));
+
+    /* Set the window's properties */
+    gtk_window_set_title(GTK_WINDOW(window), app_name);
+    gtk_window_set_default_size(GTK_WINDOW(window), 640, 480);
+
+    /*
+        Present the window: 
+        Display the window if it wasn't already displayed + Place it above all the other windows of this app.
+    */
+    gtk_window_present(GTK_WINDOW(window));
+
+    g_print("The \"%s\" GtkApplication is activated.\n", app_name);
     return;
 }
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
     GtkApplication* app;
     int status;
